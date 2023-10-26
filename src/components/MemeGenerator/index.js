ad32b022500e2dae24dd7537f4ebe0e97a7ecf05
SachinBalagam <balagamsachin337@gmail.com>
2023-10-26 15:36:56 +0000

    Final Commit

--- src/components/MemeGenerator/index.js
+++ src/components/MemeGenerator/index.js
@@ -1,3 +1,16 @@
+import {Component} from 'react'
+import {
+  LabelComponent,
+  InputComponent,
+  HeadingComponent,
+  FormComponent,
+  BgContainerComponent,
+  OptionComponent,
+  ButtonComponent,
+  OutputContainer,
+  Paragraph,
+} from './styledComponents'
+
 const fontSizesOptionsList = [
   {
     optionId: '8',
@@ -29,3 +42,84 @@ const fontSizesOptionsList = [
   },
 ]
 // Write your code here
+
+class MemeGenerator extends Component {
+  state = {
+    bottomText: '',
+    topText: '',
+    imageUrl: '',
+    fontSize: fontSizesOptionsList[0].optionId,
+    isGenerated: false,
+  }
+
+  onImageUrlChange = event => {
+    this.setState({imageUrl: event.target.value})
+  }
+
+  onTopTextChange = event => {
+    this.setState({topText: event.target.value})
+  }
+
+  onBottomTextChange = event => {
+    this.setState({bottomText: event.target.value})
+  }
+
+  onFontSizeChange = event => {
+    this.setState({fontSize: event.target.value})
+  }
+
+  onSubmitForm = event => {
+    event.preventDefault()
+    this.setState({isGenerated: true})
+  }
+
+  render() {
+    const {fontSize, bottomText, topText, imageUrl, isGenerated} = this.state
+    return (
+      <BgContainerComponent>
+        <FormComponent onSubmit={this.onSubmitForm}>
+          <HeadingComponent>Meme Generator</HeadingComponent>
+          <LabelComponent htmlFor="image">Image Url</LabelComponent>
+          <InputComponent
+            id="image"
+            placeholder="Enter Image Url"
+            onChange={this.onImageUrlChange}
+            value={imageUrl}
+          />
+
+          <LabelComponent htmlFor="topText">Top Text</LabelComponent>
+          <InputComponent
+            id="topText"
+            placeholder="Enter Top Text"
+            onChange={this.onTopTextChange}
+            value={topText}
+          />
+
+          <LabelComponent htmlFor="bottomText">Bottom Text</LabelComponent>
+          <InputComponent
+            id="bottomText"
+            placeholder="Enter Bottom Text"
+            onChange={this.onBottomTextChange}
+            value={bottomText}
+          />
+
+          <LabelComponent htmlFor="bottomText">Font Size</LabelComponent>
+          <OptionComponent onChange={this.onFontSizeChange} value={fontSize}>
+            {fontSizesOptionsList.map(each => (
+              <option key={each.optionId}>{each.displayText}</option>
+            ))}
+          </OptionComponent>
+          <ButtonComponent type="submit">Generate</ButtonComponent>
+        </FormComponent>
+        {isGenerated && (
+          <OutputContainer bgImage={imageUrl} data-testid="meme">
+            <Paragraph size={fontSize}>{topText}</Paragraph>
+            <Paragraph size={fontSize}>{bottomText}</Paragraph>
+          </OutputContainer>
+        )}
+      </BgContainerComponent>
+    )
+  }
+}
+
+export default MemeGenerator
